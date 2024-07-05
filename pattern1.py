c10a266bdbe66f2aff92b5d48a0f9783194729df
Anshika <anshikag885@gmail.com>
2024-07-05 18:57:28 +0530

    #6 creation,indexing,slicing,validation,formatting

--- pattern1.py
+++ pattern1.py
@@ -1,8 +1,12 @@
 from turtle import * 
-# square
-side = 8
-for i in range(side):
-    fd(120)
-    rt(360/side)
-print(i)
+fillcolor("red")
+begin_fill()
+for i in range(5):
+    fd(100)
+    lt(72)
+end_fill()
+fillcolor("yellow")
+begin_fill()
+circle(50)
+end_fill()
 mainloop()
